f4cd96a6f2d50e84c600c2d9024470ee8b0055a6
Tubha Ahmad <toubha.ah@hotmail.com>
2026-02-18 15:11:01 +0100

    Add Tanstack Query provider

--- jsf-ca-tubha/src/App.tsx
+++ jsf-ca-tubha/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
@@ -7,20 +8,23 @@ import CartPage from "./pages/CartPage";
 import ContactPage from "./pages/ContactPage";
 import Layout from "./components/Layout";
 
-function App() {
+const queryClient = new QueryClient();
+
+// QueryClientProvider is used to provide the QueryClient to the entire app, allowing us to use React Query for data fetching and caching. //
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="product/:id" element={<ProductPage />} />
-          <Route path="checkout-success" element={<CheckoutSuccessPage />} />
-          <Route path="cart" element={<CartPage />} />
-          <Route path="contact" element={<ContactPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="product/:id" element={<ProductPage />} />
+            <Route path="checkout-success" element={<CheckoutSuccessPage />} />
+            <Route path="cart" element={<CartPage />} />
+            <Route path="contact" element={<ContactPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
-
-export default App;
